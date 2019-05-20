98dc65063fd2662a427b4733f0cb754c0be7dd04
cu3l3bre <cu3l3bre@googlemail.com>
2019-05-20 12:22:18 +0200

    Projektdateien hinzufügen.

---
+++ Vererbung3/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include "weltraum.h"
+
+
+
+using namespace std;
+
+
+int main()
+{
+	cout << "Programm zu Vererbung3" << endl << endl;
+
+
+
+
+	system("pause");
+	return 0;
+}
---
+++ Vererbung3/weltraum.h
@@ -0,0 +1,41 @@
+#pragma once
+
+
+// eigentlich bekommt jede Klasse ihre eigene Header-Datei und falls erforderlich eine eigene cpp Datei
+
+// allgemeine Klasse - Basisklasse 
+
+class Himmelskoerper
+{
+public:
+	double masse;	// Einhet???
+	double radius;	// km 
+
+};
+
+
+// spezialisierte Klasse
+class Stern : public Himmelskoerper
+{
+public:
+	double Leuchtkraft;	// Enheit ???
+
+};
+
+
+
+class Planet : public Himmelskoerper
+{
+public:
+	bool istBewohnt;	// gibt an, ob auf dem Planeten Leben vorhanden ist
+};
+
+
+
+class Mond : public Himmelskoerper
+{
+public:
+	double Umlaufzeit;	// Einheit???
+
+
+};
